8b87f8dcfee03192bd89ac8e3956c7c5a89c8ec6
Serenitea <themaryzhou@gmail.com>
2021-04-11 01:13:06 -0700

    Update app.js

--- express-main-example/express/app.js
+++ express-main-example/express/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const routes = {
-	users: require('./routes/users'),
-	instruments: require('./routes/instruments'),
-	orchestras: require('./routes/orchestras'),
-	// Add more routes here...
-	// items: require('./routes/items'),
+    users: require('./routes/users'),
+    goalJobsApplied: require('./routes/goalJobsApplied'),
+    team: require('./routes/team'),
+    // Add more routes here...
+    // items: require('./routes/items'),
 };
 
 const app = express();
@@ -16,18 +16,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // We create a wrapper to workaround async errors not being transmitted correctly.
 function makeHandlerAwareOfAsyncErrors(handler) {
-	return async function(req, res, next) {
-		try {
-			await handler(req, res);
-		} catch (error) {
-			next(error);
-		}
-	};
+    return async function(req, res, next) {
+        try {
+            await handler(req, res);
+        } catch (error) {
+            next(error);
+        }
+    };
 }
 
 // We provide a root route just as an example
 app.get('/', (req, res) => {
-	res.send(`
+    res.send(`
 		<h2>Hello, Sequelize + Express!</h2>
 		<p>Make sure you have executed <b>npm run setup-example-db</b> once to have a populated example database. Otherwise, you will get <i>'no such table'</i> errors.</p>
 		<p>Try some routes, such as <a href='/api/users'>/api/users</a> or <a href='/api/orchestras?includeInstruments'>/api/orchestras?includeInstruments</a>!</p>
@@ -37,36 +37,36 @@ app.get('/', (req, res) => {
 
 // We define the standard REST APIs for each route (if they exist).
 for (const [routeName, routeController] of Object.entries(routes)) {
-	if (routeController.getAll) {
-		app.get(
-			`/api/${routeName}`,
-			makeHandlerAwareOfAsyncErrors(routeController.getAll)
-		);
-	}
-	if (routeController.getById) {
-		app.get(
-			`/api/${routeName}/:id`,
-			makeHandlerAwareOfAsyncErrors(routeController.getById)
-		);
-	}
-	if (routeController.create) {
-		app.post(
-			`/api/${routeName}`,
-			makeHandlerAwareOfAsyncErrors(routeController.create)
-		);
-	}
-	if (routeController.update) {
-		app.put(
-			`/api/${routeName}/:id`,
-			makeHandlerAwareOfAsyncErrors(routeController.update)
-		);
-	}
-	if (routeController.remove) {
-		app.delete(
-			`/api/${routeName}/:id`,
-			makeHandlerAwareOfAsyncErrors(routeController.remove)
-		);
-	}
+    if (routeController.getAll) {
+        app.get(
+            `/api/${routeName}`,
+            makeHandlerAwareOfAsyncErrors(routeController.getAll)
+        );
+    }
+    if (routeController.getById) {
+        app.get(
+            `/api/${routeName}/:id`,
+            makeHandlerAwareOfAsyncErrors(routeController.getById)
+        );
+    }
+    if (routeController.create) {
+        app.post(
+            `/api/${routeName}`,
+            makeHandlerAwareOfAsyncErrors(routeController.create)
+        );
+    }
+    if (routeController.update) {
+        app.put(
+            `/api/${routeName}/:id`,
+            makeHandlerAwareOfAsyncErrors(routeController.update)
+        );
+    }
+    if (routeController.remove) {
+        app.delete(
+            `/api/${routeName}/:id`,
+            makeHandlerAwareOfAsyncErrors(routeController.remove)
+        );
+    }
 }
 
 module.exports = app;
